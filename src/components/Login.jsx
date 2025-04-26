36f190dddc9e19ea557ffdc6d8457b7132fdf3af
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-26 21:55:56 +0530

    setup redux store

---
+++ src/components/Login.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("ajitkumar@gmail.com");
+  const [password, setPassword] = useState("Ajit@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      //console.log(res?.data);
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend text-xl">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="text"
+          value={emailId}
+          className="input"
+          onChange={(e) => setEmailId(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="text"
+          value={password}
+          className="input"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
