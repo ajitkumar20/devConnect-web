d45a8839da5160048e88b76d0b18d50f9a722c3b
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-29 11:55:11 +0530

    feat: send/ignore the user card from the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,6 +25,14 @@ const Feed = () => {
     getFeed();
   }, []);
 
+  if (!feed) return;
+  if (feed.length <= 0)
+    return (
+      <h1 className="flex justify-center my-7 font-bold text-2xl">
+        No new users found!!!
+      </h1>
+    );
+
   return (
     feed && (
       <div className="flex justify-center my-10">
